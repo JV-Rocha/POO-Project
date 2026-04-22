5dccf3ddb83356aab94f2b798e1d251fda6eca2e
João Victor Rocha <138988583+JV-Rocha@users.noreply.github.com>
2026-04-21 21:59:02 -0300

    Implement Singleton pattern for GameClient
    Added Singleton pattern implementation for GameClient to manage a game library.

--- FERIADAOCOMPADROES.py
+++ FERIADAOCOMPADROES.py
@@ -9,7 +9,7 @@ class Game(ABC):
 # Implementações
 class IndieGame(Game):
     def play(self):
-        return "Jogando Hollow Knight
+        return "Jogando Hollow Knight"
 
 class AAAGame(Game):
     def play(self):
@@ -27,9 +27,31 @@ class GameFactory:
             raise ValueError("Tipo inválido")
 
 
+# Singleton
+class GameClient:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GameClient, cls).__new__(cls)
+            cls._instance.library = []
+        return cls._instance
+    
+    def add_game(self, game):
+        self.library.append(game)
+
+    def show_library(self):
+        return self.library
+
 # Uso
 game1 = GameFactory.create_game("indie")
 game2 = GameFactory.create_game("aaa")
 
+client1 = GameClient()
+client1.add_game("Hollow Knight")
+client2 = GameClient()
+
 print(game1.play())
 print(game2.play())
+
+print(client2.show_library())
